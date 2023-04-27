ee496b2052293c60d762fda62cab12e0f7324a96
Stubtoe <peterson.adam23@gmail.com>
2023-04-27 16:34:17 -0400

    push

---
+++ src/CardLists/Comments-Apeterson.tsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import Card from "../componets/GeneriCard";
+
+type Comment = {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+};
+
+const CommentList = () => {
+  const [comments, setComments] = useState<Comment[]>([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/comments")
+      .then((response) => response.json())
+      .then((data) => {
+        setComments(data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  const cardConfigs = comments
+    .slice(0, 15)
+    .map((comment) => ({
+      key: `card${comment.id}`,
+      title: `Comment ${comment.id}`,
+      url:`/comments/${comment.id}`,
+      children: (
+        <>
+          <p>Name: {comment.name}</p>
+          <p>Email: {comment.email}</p>
+          <p>Body: {comment.body}</p>
+        </>
+      ),
+    }));
+
+  return (
+    <div style={{ display: "flex", flexWrap: "wrap" }}>
+      {cardConfigs.map(({ key, ...cardProps }) => (
+        <div key={key} style={{ flex: "1 0 0" }}>
+          <Card {...cardProps} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CommentList;
